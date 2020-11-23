9872354fbba61bce957a8526c179cbf604a7ed16
Malak Majeed Ullah khan <malak310512@gmail.com>
2020-11-23 14:47:05 +0500

    Add files via upload

---
+++ Lab07.srcs/sources_1/new/Trig_simulation.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/22/2020 01:09:25 PM
+// Design Name: 
+// Module Name: Trig_simulation
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Trig_simulation(
+
+    );
+endmodule
